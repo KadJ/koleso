b1189bf9dfa2df5ffa3f64050c93754e7ced1ddc
KadJ <rtwka@mail.ru>
2014-04-06 15:59:14 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koleso::Application.config.secret_key_base = '2754f1800d4e9989f8d7ef7ef3b2a6224d18fd788cb2b2a9186b647ccd705e0872cb13de2884e749323e2751e6aa0c9a2e8ac00c4d6338ae41bf6a47e6b4a9f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Koleso::Application.config.session_store :cookie_store, key: '_koleso_session'
